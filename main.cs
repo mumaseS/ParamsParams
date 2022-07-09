d3e54c98089762838ca0598725ddd15078d8a511
mumaseS <106332421+mumaseS@users.noreply.github.com>
2022-07-09 13:17:41 +0000

    lesson 5.5.8

--- main.cs
+++ main.cs
@@ -2,29 +2,24 @@ using System;
 
 class Program {
   public static void Main (string[] args) 
+  { 
+     Console.WriteLine(PowerUp(2,3));
+  }
+  private static int PowerUp(int N, byte pow)
   {
-  Console.WriteLine("Напишите что-то");
-  var str = Console.ReadLine();
-
-  Console.WriteLine("Укажите глубину эха");
-  var deep = int.Parse(Console.ReadLine());
-Echo(str, deep);
-    }
-
-static void Echo(string saidword, int deep) {
- var modif = saidword;    
-  
-  if (modif.Length > 2) 
+    if (pow == 0)
     {
-      modif = modif.Remove(0, 2);
+      return 1;
     }
-  Console.BackgroundColor = (ConsoleColor)deep;
-  Console.WriteLine("..." + modif);
-  if (deep > 1) 
-  {
-    Echo(modif, deep - 1);
-  }
-  
-    
-  }
+    else 
+    {
+    if (pow == 1)
+    {
+      return N ;
+    }
+      else 
+      {
+        return N * PowerUp(N, --pow);
+      }
+  }}
 }
